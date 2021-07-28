d6b069b3db96a0a877eeafd8193d89f8ab73df2f
MANOEL <manoel.programer@gmail.com>
2021-07-28 18:53:12 -0300

    criada a pagina de contato e parceria

--- src/components/Contato/index.js
+++ src/components/Contato/index.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React from "react";
+import './style.css';
 
+function Contato() {
+  return (
+    <>
+      <h1>CONTATO</h1>
+      
+      <div className="container-contato">
+      <h3 className="container-texto">
+        A DUX LEGALSOLUTIONS está sempre aberta para você e sua empresa. Envie
+        uma mensagem para nosso e-mail ou entre em contato com o nosso
+        escritório.
+      </h3>
+       <form>
+           <div className="container-form">
+           <label>
+            NOME:
+           <input type="text" />
+           </label>
 
-function Contato(){
-    return(
-        <>
-            <h4>PAGE CONTATO</h4>
-        </>
-    );
+           <label>
+            E-mail:
+           <input type="email" />
+           </label>
 
+           <label>
+            TELEFONE:
+           <input type="text" />
+           </label>
+
+           <label>
+            ASSUNTO:
+           <input type="text" />
+           </label>
+            
+            <label>
+                MENSAGEM:
+                <textarea type="text" rows="20"/>
+            </label>
+
+            <button type="submit">ENVIAR</button>
+            
+           </div>
+                
+        </form>
+
+     
+        
+       
+        
+
+      </div>
+    </>
+  );
 }
 
 export default Contato;
--- src/components/Parceria/index.js
+++ src/components/Parceria/index.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React from "react";
 
+function Parceria() {
+  return (
+    <>
+      
 
-function Parceria(){
-    return(
-        <>
-            <h4>PAGE PARCERIA</h4>
-        </>
-    );
+      <div className="container-contato">
+      <h1 className="container-texto">PARCERIA</h1>
+        <h3 className="container-texto">
+          SEJA VOCE TAMBEM O NOSSO PARCEIRO, SEJA UM CONSULTOR DUX LEGAL
+          SOLUTIONS{" "}
+        </h3 >
+        <h4 className="container-texto">ATUAÇÃO COM AUTONOMA E INDEPENDENCIA FINANCEIRA</h4>
+        <p className="container-texto">
+          A Dux Legal Solutions nao para de crescer e busca constantemente
+          expandir e fortalecer a sua rede de apoio e atuaçao geografica.
+          Estamos sempre em busca de parceiros, profissionais liberais ou pessoa
+          juridica, que possam contribuir na expansao dos nossos serviços em
+          nossos terrirorios de atuacao. Se voce è advogado, administrador,
+          contador ou tenha qualquer função que englobe uma carteira de clientes
+          ou uma ampla rede social, poderá trabalhar em parceira conosco. O
+          perfil que procuramos é de pessoas sérias, íntegras, comprometidas com
+          o bom atendimento e respeito aos nossos principios e clientes.
+          Qualquer que seja sua área de atuação, poderá nos apoiar em sua
+          região, estado ou cidade. Caso haja interesse, favor preencher e
+          enviar o formulário ao lado, então entraremos em contato para agendar
+          uma videoconferencia, a fim de avaliar a possibilidade de aprofundar o
+          conhecimento do seu perfil e fornecer as orientações gerais sobre
+          nosso modo de atuação.
+        </p>
+        <form>
+          <div className="container-form">
+            <h2>FORMULÁRIO DE PROPOSTA DE PARCERIA</h2>
+            <label>
+              NOME:
+              <input type="text" />
+            </label>
 
+            <label>
+              E-MAIL:
+              <input type="email" />
+            </label>
+
+            <label>
+              TELEFONE:
+              <input type="text" />
+            </label>
+
+            <label>
+              CELULAR:
+              <input type="text" />
+            </label>
+
+            
+
+            <button type="submit">ENVIAR</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Parceria;
